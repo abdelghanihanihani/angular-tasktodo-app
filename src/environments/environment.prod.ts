7b5c10f1d75804effc46e8e846e6d1deb96fff50
abdelghanihani <40170398+abdelghanihanihani@users.noreply.github.com>
2021-05-20 22:22:28 +0100

    generated deployables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDnItUH0AaJw_ErNpSWRg6VExryxIxv3MA',
+    authDomain: 'angular-tasktodo-app.firebaseapp.com',
+    projectId: 'angular-tasktodo-app',
+    storageBucket: 'angular-tasktodo-app.appspot.com',
+    messagingSenderId: '57636770904',
+    appId: '1:57636770904:web:e28e3c397755bae69aa62d'
+  }
+
 };
