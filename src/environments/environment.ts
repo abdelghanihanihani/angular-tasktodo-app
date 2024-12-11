cd386c1c77f607ae13e31bad1e1db5214d367b69
abdelghanihanihani <hani.abdelghanigit@gmail.com>
2024-12-11 16:18:51 +0400

    change firebase key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: 'AIzaSyDnItUH0AaJw_ErNpSWRg6VExryxIxv3MA',
+    apiKey: 'AIzaSyByZMfhPNT9G7h9Qlsr8PhiY8LgVyvqjHA',
     authDomain: 'angular-tasktodo-app.firebaseapp.com',
     projectId: 'angular-tasktodo-app',
     storageBucket: 'angular-tasktodo-app.appspot.com',
     messagingSenderId: '57636770904',
-    appId: '1:57636770904:web:e28e3c397755bae69aa62d'
+    appId: '1:57636770904:web:75bcc23e933d43339aa62d'
   }
 
 };
